4c409b7e78b7f85dfe5c0bfbdaf85b95f42619c1
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2023-04-30 20:31:19 +0900

    Update HelloGit.java

--- HelloGit/src/kr/co/ezenac/git/HelloGit.java
+++ HelloGit/src/kr/co/ezenac/git/HelloGit.java
@@ -3,9 +3,9 @@ package kr.co.ezenac.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
-		System.out.println("What's Git?");
-		System.out.println("That's DVCS!");
+		System.out.println("Hello Git!!!!!");
+		System.out.println("What's Git?????");
+		System.out.println("That's DVCS!!!!");
 
 		
 	}
